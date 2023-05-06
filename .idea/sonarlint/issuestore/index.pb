95f8a6b5039106c0c50aca622408a100e8566b1c
MrAzimzadeh <mehemmedezimzase@gmail.com>
2023-05-06 10:23:47 +0400

    Update

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,7 @@
 
 @
 app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
-P
- app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
-p
-@app/src/main/java/com/azimzada/bedenkutleindeksi/MainActivity.kt,3\6\363eed20d86f78458b6570e7ffe5ad9becdba04c
+s
+Capp/src/test/java/com/azimzada/bedenkutleindeksi/ExampleUnitTest.kt,2\c\2c0ec4b41aebb7173ca83b13a26188dab57022fa
+‚
+Rapp/src/androidTest/java/com/azimzada/bedenkutleindeksi/ExampleInstrumentedTest.kt,2\5\253a20dc7366659fb541829dd01f62abc80909e8
--- app/src/main/java/com/azimzada/bedenkutleindeksi/MainActivity.kt
+++ app/src/main/java/com/azimzada/bedenkutleindeksi/MainActivity.kt
@@ -8,27 +8,31 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        binding.weighttNum.minValue = 30
-        binding.weighttNum.maxValue = 250
+        binding.weighttNum.minValue = 10
+        binding.weighttNum.maxValue = 300
 
-        binding.heightNum.minValue = 100
-        binding.heightNum.maxValue = 250
+        binding.heightNum.minValue = 10
+        binding.heightNum.maxValue = 300
 
 
         binding.weighttNum.setOnValueChangedListener { _, _, _ ->
             calculateBMI()
         }
+
         binding.heightNum.setOnValueChangedListener { _, _, _ ->
             calculateBMI()
         }
 
+
     }
 
     fun calculateBMI() {
+
         val height = binding.heightNum.value
         val toDoubleHeight = height.toDouble() / 100
 
@@ -36,24 +40,25 @@ class MainActivity : AppCompatActivity() {
 
         val bmi = weight.toDouble() / (toDoubleHeight * toDoubleHeight)
 
-        binding.tvResult.text = String.format("Sizin bədən kütlə indeksiniz : %.2f ", bmi)
+        binding.tvResult.text = String.format(":  %.2f", bmi)
+
         binding.helthyTV.text = String.format("Məlumat %s", healityMessage(bmi))
+
     }
 
     fun healityMessage(bmi: Double): String {
+
         if (bmi < 18.5) {
-            return " - Boy uzunluğunuza görə bədən çəkiniz normalın altındadır və bu mövzuda daha çox məlumat əldə etmək üçün dietoloqa müraciət etməyinizi tövsiyə edirik."
+            return " Boy uzunluğunuza görə bədən çəkiniz normalın altındadır və bu mövzuda daha çox məlumat əldə etmək üçün dietoloqa müraciət etməyinizi tövsiyə edirik."
         }
         if (bmi < 24.9) {
-            return " - Boy uzunluğunuza görə bədən çəkiniz \"normal\"–dır. Sağlam və balanslı qidalanaraq, həmçinin nizamlı fiziki fəaliyyət edərək bu çəkinizi qorumağa çalışın."
+            return " Boy uzunluğunuza görə bədən çəkiniz \"normal\"–dır. Sağlam və balanslı qidalanaraq, həmçinin nizamlı fiziki fəaliyyət edərək bu çəkinizi qorumağa çalışın."
         }
         if (bmi < 29.9) {
-            return " - Boy uzunluğunuza görə bədən çəkiniz \"normaldan çoxdur\". Bu mövzuda daha çox məlumat əldə etmək üçün dietoloqa müraciət etməyinizi tövsiyə edirik."
+            return " Boy uzunluğunuza görə bədən çəkiniz \"normaldan çoxdur\". Bu mövzuda daha çox məlumat əldə etmək üçün dietoloqa müraciət etməyinizi tövsiyə edirik."
         }
         if (bmi < 30.0)
-            return " - Boy uzunluğunuza görə bədən çəkiniz normalın çox üstündədir və \"Piylənmə\" dərəcəsinə girir. Köklük, ürək-damar xəstəlikləri, diabet, hipertoniya v.s. xroniki xəstəliklər üçün risk faktorudur. Həkim / dietoloq nəzarətində arıqlayaraq normal çəkinizi əldə etməyiniz sağlamlığınız baxımından çox əhəmiyyətlidir. "
-
-        return "Obez"
+            return " Boy uzunluğunuza görə bədən çəkiniz normalın çox üstündədir və \"Piylənmə\" dərəcəsinə girir. Köklük, ürək-damar xəstəlikləri, diabet, hipertoniya v.s. xroniki xəstəliklər üçün risk faktorudur. Həkim / dietoloq nəzarətində arıqlayaraq normal çəkinizi əldə etməyiniz sağlamlığınız baxımından çox əhəmiyyətlidir. "
+        return "\"Obez\""
     }
-
 }
